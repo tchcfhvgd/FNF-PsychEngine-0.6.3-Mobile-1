feec6b5a17ae04031d0790e02f3f890f95ef3d5f
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-12-21 15:17:47 +0800

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -338,14 +338,14 @@ class TitleState extends MusicBeatState
 
 		titleText = new FlxSprite(titleJSON.startx, titleJSON.starty);
 		#if MODS_ALLOWED
-		var path = "mods/" + Paths.currentModDirectory + "/images/titleEnter.png";
+		var path = Sys.getCwd() + "mods/" + Paths.currentModDirectory + "/images/titleEnter.png";
 		//trace(path, FileSystem.exists(path));
 		if (!FileSystem.exists(path)){
-			path = "mods/images/titleEnter.png";
+			path = Sys.getCwd() + "mods/images/titleEnter.png";
 		}
 		//trace(path, FileSystem.exists(path));
 		if (!FileSystem.exists(path)){
-			path = "assets/images/titleEnter.png";
+			path = Sys.getCwd() + "assets/images/titleEnter.png";
 		}
 		//trace(path, FileSystem.exists(path));
 		titleText.frames = FlxAtlasFrames.fromSparrow(BitmapData.fromFile(path),File.getContent(StringTools.replace(path,".png",".xml")));
