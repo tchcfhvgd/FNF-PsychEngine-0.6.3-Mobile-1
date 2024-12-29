3c4432f0fbd73f32b5cc40b98ea3e8f80ef50b90
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-12-29 21:32:10 +0800

    Update ClientPrefs.hx

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -67,7 +67,7 @@ class ClientPrefs {
 		'opponentplay' => false
 	];
 
-	public static var comboOffset:Array<Int> = [200, 200, 200, 200];
+	public static var comboOffset:Array<Int> = [-200, -200, -200, -200];
 	public static var ratingOffset:Int = 0;
 	public static var sickWindow:Int = 45;
 	public static var goodWindow:Int = 90;
